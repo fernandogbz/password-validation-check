b787aede642652dbbe5d0dff0688c678ed757325
Fernando Jimenez <fgjv06@gmail.com>
2023-04-25 06:39:22 -0400

    JS - Toggle the password input type between 'password' and 'text'

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const passwordInput = document.querySelector(".pass-field input");
 const eyeIcon = document.querySelector(".pass-field i");
 
-eyeIcon.addEventListener("click"), () => {
-  
-}
+eyeIcon.addEventListener("click", () => {
+  // Toggle the password input type between "password" and "text"
+  passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+});
