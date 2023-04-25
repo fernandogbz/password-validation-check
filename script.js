64484818bbae36d2238b9ca821d6296db23f8281
Fernando Jimenez <fgjv06@gmail.com>
2023-04-25 06:35:13 -0400

    JS - eyeIcon addEventListener

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 // Getting variables
 const passwordInput = document.querySelector(".pass-field input");
 const eyeIcon = document.querySelector(".pass-field i");
+
+eyeIcon.addEventListener("click"), () => {
+  
+}
