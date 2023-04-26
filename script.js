98f407afad002b8a1e7ef3f5f1499b4c893e79c1
Fernando Jimenez <fgjv06@gmail.com>
2023-04-26 18:53:56 -0400

    JS - Updating icon of requirement item if requirement matched or not

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const requirementList = document.querySelectorAll(".requirement-list li");
 // An array of password requirements with corresponding regular expressions and index of the requirement list item
 const requirements = [
   {regex: /.{8,}/, index:0 }, // Minimum of 8 characters
-  {regex: /.[0-9]/, index:1 }, // At least one number
-  {regex: /.[a-z]/, index:2 }, // At least one  lowercase letter
-  {regex: /.[A-Z]/, index:3 }, // At least one  uppercase letter
-  {regex: /.[^A-Za-z0-9]/, index:4 }, // At least one special character 
+  {regex: /[0-9]/, index:1 }, // At least one number
+  {regex: /[a-z]/, index:2 }, // At least one  lowercase letter
+  {regex: /[A-Z]/, index:3 }, // At least one  uppercase letter
+  {regex: /[^A-Za-z0-9]/, index:4 }, // At least one special character 
 ]
 
 passwordInput.addEventListener("keyup", (e)=>{
@@ -17,7 +17,17 @@ passwordInput.addEventListener("keyup", (e)=>{
   requirements.forEach(item => {
     //Check if the password matches the requirement regex
     const isValid = item.regex.test(e.target.value);
-    
+    const requirementItem = requirementList[item.index];
+
+    // Getting a particular element of object index number
+    // Updating icon of requirement item if requirement matched or not
+    if(isValid) {
+      // firstElementChild returns the first child of li, which is the icon element
+      requirementItem.firstElementChild.className = "fa-solid fa-check";
+    } else {
+      requirementItem.firstElementChild.className = "fa-solid fa-circle";
+      
+    }
   });
 });
 
