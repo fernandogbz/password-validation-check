731846d39836bb775addb04c69e74d5b9d9a2079
Fernando Jimenez <fgjv06@gmail.com>
2023-04-26 06:11:34 -0400

    JS - Update the eye icon class based on the password input type

--- script.js
+++ script.js
@@ -5,4 +5,7 @@ const eyeIcon = document.querySelector(".pass-field i");
 eyeIcon.addEventListener("click", () => {
   // Toggle the password input type between "password" and "text"
   passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+
+  // Update the eye icon class based on the password input type
+  eyeIcon.className = `fa-solid fa-eye${passwordInput.type === "password" ? "" : "-slash"}`;
 });
