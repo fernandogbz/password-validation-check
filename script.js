950e700c19a96941560fc02675be48ee8e4eb3b3
Fernando Jimenez <fgjv06@gmail.com>
2023-04-26 18:38:37 -0400

    JS - checking if the password matches the requirement regex

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 // Getting variables
 const passwordInput = document.querySelector(".pass-field input");
 const eyeIcon = document.querySelector(".pass-field i");
+const requirementList = document.querySelectorAll(".requirement-list li");
 
 // An array of password requirements with corresponding regular expressions and index of the requirement list item
 const requirements = [
-  {regex: /.{8,}/, index:0 },
-  {regex: /.[0-9]/, index:1 },
-  {regex: /.[a-z]/, index:2 },
-  {regex: /.[A-Z]/, index:3 },
-  {regex: /.[^A-Za-z0-9]/, index:4 },
+  {regex: /.{8,}/, index:0 }, // Minimum of 8 characters
+  {regex: /.[0-9]/, index:1 }, // At least one number
+  {regex: /.[a-z]/, index:2 }, // At least one  lowercase letter
+  {regex: /.[A-Z]/, index:3 }, // At least one  uppercase letter
+  {regex: /.[^A-Za-z0-9]/, index:4 }, // At least one special character 
 ]
 
-passwordInput.addEventListener("keyup", ()=>{
+passwordInput.addEventListener("keyup", (e)=>{
+  //Looping through each object item of the array
+  requirements.forEach(item => {
+    //Check if the password matches the requirement regex
+    const isValid = item.regex.test(e.target.value);
+    
+  });
 });
 
 
